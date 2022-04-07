e54972b65c0ba9f4000a56eff3282cb7d38f8a2b
chrisais9 <chrisais9@naver.com>
2022-04-07 17:16:33 +0900

    :sparkles: config와 assets를 기반으로 랜덤 NFT와 메타데이터를 생성해주는 기능 구현완료

--- generator.py
+++ generator.py
@@ -2,57 +2,89 @@ from PIL import Image
 from image4layer import Image4Layer
 import yaml
 import random
+import json
 
 
 class Generator:
 
     def __init__(self, config):
+        self.traits = []
         self.config = config
+        self.final_images = []
+        self.width = 2000
+        self.height = 2000
 
-    def test(self):
-        im1 = Image.open(f'traits/1.png').convert('RGBA')
-        im2 = Image.open(f'traits/2.png').convert('RGBA')
-        im3 = Image.open(f'traits/3.png').convert('RGBA')
-        im4 = Image.open(f'traits/4.png').convert('RGBA')
-        im5 = Image.open(f'traits/5.png').convert('RGBA')
-        im6 = Image.open(f'traits/6.png').convert('RGBA')
-        im7 = Image.open(f'traits/7.png').convert('RGBA')
-        im8 = Image.open(f'traits/8.png').convert('RGBA')
+    def generate(self):
 
-        stack = Image.alpha_composite(im1, im2)
-        stack = Image.alpha_composite(stack, im3)
-        stack = Image.alpha_composite(stack, im4)
-        stack = Image.alpha_composite(stack, im5)
-        stack = Image.alpha_composite(stack, im6)
-        stack = Image.alpha_composite(stack, im7)
-        stack = Image.alpha_composite(stack, im8)
+        self.traits = self.generate_traits()
 
-        filter = Image.open("traits/watercolor.png").convert("RGBA")
+        if self.is_all_trait_unique(self.traits):
+            print("Confirmed that every single trait is unique")
+        else:
+            print("Error! There are duplicated trait")
+            return
+
+        self.generate_all_trait_metadata()
+
+        for trait in self.traits:
+            image = self.generate_image(trait)
+            image.show()
+
+    def generate_traits(self):
+        traits = []
+        for i in range(self.config["number"]):
+            new_trait = self.new_random_unique_trait()
+            traits.append(new_trait)
+        return traits
+
+    def new_random_unique_trait(self):
+        trait = {}
+
+        traits = self.config["traits"]
+        categories = list(traits.keys())
+        for category in categories:
+            trait[category] = random.choices(
+                [k for k in traits[category]["values"].keys()],
+                [v["prob"] for v in traits[category]["values"].values()]
+            )[0]
+
+        if trait in self.traits:
+            return self.new_random_unique_trait()
+        else:
+            return trait
+
+    def is_all_trait_unique(self, traits):
+        seen = list()
+        return not any(i in seen or seen.append(i) for i in traits)
+
+    def generate_all_trait_metadata(self):
+        METADATA_FILE_NAME = './metadata/all-traits.json'
+        with open(METADATA_FILE_NAME, 'w') as outfile:
+            json.dump(self.traits, outfile, indent=4)
+
+    def generate_image(self, trait):
+        stack = Image.new('RGBA', (self.width, self.height))
+
+        for category, name in trait.items():
+            image_layer = Image.open(f'{self.config["traits"][category]["values"][name]["src"]}').convert('RGBA')
+            stack = Image.alpha_composite(stack, image_layer)
+
+        filter = Image.open("image/filter.png").convert("RGBA")
         filter.putalpha(int(256 * 0.3))
+        stack = Image4Layer.pin_light(stack, filter)
 
-        final = Image4Layer.pin_light(stack, filter)
-
-        final.show()
-
-    def load_traits(self):
-        for idx, (trait_id, trait) in enumerate(self.config["traits"].items()):
-            items = trait["values"]
-            print("Category:", trait["category"])
-            for idx, (item, properties) in enumerate(items.items()):
-                print(idx)
-                print("item:", item)
-                print("properties:", properties)
-            print("\n")
+        stack = stack.convert('RGB')
+        return stack
 
 
 def main():
     with open('config.yaml') as config:
         config = yaml.load(config, Loader=yaml.FullLoader)
 
-    random.seed(config["seed"])
+    # random.seed(config["seed"])
 
     generator = Generator(config)
-    generator.load_traits()
+    generator.generate()
 
 
 if __name__ == '__main__':
